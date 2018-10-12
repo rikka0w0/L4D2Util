f631b6952e13b055270d75857ca48f024bb7d976
rikka0w0 <929513338@qq.com>
2018-10-12 15:09:48 +1100

    Rename app and change icon

---
+++ app/src/main/java/rikka/srcdsutil/common/L4D2Server.java
@@ -0,0 +1,152 @@
+package rikka.srcdsutil.common;
+
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+
+/**
+ * Created by Administrator on 2/13/2018.
+ */
+
+public class L4D2Server {
+    private final static byte[] requestQueryServerInfo = {
+            (byte) 0xff, (byte) 0xff, (byte) 0xff, (byte) 0xff, 0x54, 0x53, 0x6f, 0x75,
+            0x72, 0x63, 0x65, 0x20, 0x45, 0x6e, 0x67, 0x69,
+            0x6e, 0x65, 0x20, 0x51, 0x75, 0x65, 0x72, 0x79,
+            0x00 };
+
+    private final static byte[] requestGetPlayerList = {
+            (byte) 0xff, (byte) 0xff, (byte) 0xff, (byte) 0xff, 0x55, 0x00, 0x00, 0x00,
+            0x00 };
+
+    public static class Info {
+        public String serverName;
+        public String mapName;
+        public String directory;
+        public String gameType;
+        public byte playerCount;
+        public byte slotCount;
+    }
+
+    private static int getSegmentLength(byte[] bytes, int offset, int length) {
+        int pos = offset;
+        for (; pos < length; pos++) {
+            if (bytes[pos] == 0)
+                break;
+        }
+        return pos - offset;
+    }
+
+    public static Info QueryServerInfo(String hostname, int port) {
+        try {
+            InetAddress hostAddr = InetAddress.getByName(hostname);
+            DatagramSocket socket = new DatagramSocket(0);
+            socket.setSoTimeout(5000);
+
+            DatagramPacket request = new DatagramPacket(requestQueryServerInfo, requestQueryServerInfo.length, hostAddr, port);
+            DatagramPacket response = new DatagramPacket(new byte[512], 512);
+            socket.send(request);
+            socket.receive(response);
+
+            int recvBufLength = response.getLength();
+            byte[] recvBuf = response.getData();
+            // Check magic number and header
+            if (    (recvBuf[0] & 0xff) != 0xff ||
+                    (recvBuf[1] & 0xff) != 0xff ||
+                    (recvBuf[2] & 0xff) != 0xff ||
+                    (recvBuf[3] & 0xff) != 0xff ||
+                    (recvBuf[4] & 0xff) != 0x49 ||
+                    (recvBuf[5] & 0xff) != 0x11) {
+                // Invalid responce from server
+                return null;
+            }
+
+            Info info = new Info();
+
+            int segmentStart = 6;
+            int segmentLength = getSegmentLength(recvBuf, segmentStart, recvBufLength);
+            info.serverName = new String(recvBuf, segmentStart, segmentLength, "UTF-8");
+            segmentStart += segmentLength + 1;
+
+            segmentLength = getSegmentLength(recvBuf, segmentStart, recvBufLength);
+            info.mapName = new String(recvBuf, segmentStart, segmentLength, "UTF-8");
+            segmentStart += segmentLength + 1;
+
+            segmentLength = getSegmentLength(recvBuf, segmentStart, recvBufLength);
+            info.directory = new String(recvBuf, segmentStart, segmentLength, "UTF-8");
+            segmentStart += segmentLength + 1;
+
+            segmentLength = getSegmentLength(recvBuf, segmentStart, recvBufLength);
+            info.gameType = new String(recvBuf, segmentStart, segmentLength, "UTF-8");
+            segmentStart += segmentLength + 1;
+
+            if (recvBuf[segmentStart] != 0x26 || recvBuf[segmentStart + 1] != 0x02) {
+                // Invalid responce from server\n
+                return null;
+            }
+
+            info.playerCount = recvBuf[segmentStart+2];
+            info.slotCount = recvBuf[segmentStart+3];
+
+            return info;
+        } catch (Exception e) {
+            return null;
+        }
+    }
+
+    public static String[] GetPlayerList(String hostname, int port) {
+        try {
+            InetAddress hostAddr = InetAddress.getByName(hostname);
+            DatagramSocket socket = new DatagramSocket(0);
+            socket.setSoTimeout(10000);
+
+            DatagramPacket request = new DatagramPacket(requestGetPlayerList, requestGetPlayerList.length, hostAddr, port);
+            DatagramPacket response = new DatagramPacket(new byte[512], 512);
+            socket.send(request);
+            socket.receive(response);
+
+            byte[] recvBuf = response.getData();
+            // Check magic number and header
+            if (    (recvBuf[0] & 0xff) != 0xff ||
+                    (recvBuf[1] & 0xff) != 0xff ||
+                    (recvBuf[2] & 0xff) != 0xff ||
+                    (recvBuf[3] & 0xff) != 0xff ||
+                    (recvBuf[4] & 0xff) != 0x41) {
+                // Invalid responce from server
+                return null;
+            }
+
+            // Reform request packet
+            recvBuf[4] = requestGetPlayerList[4];
+            request = new DatagramPacket(recvBuf, recvBuf.length, hostAddr, port);
+            socket.send(request);
+            socket.receive(response);
+
+            int recvBufLength = response.getLength();
+            recvBuf = response.getData();
+            // Check magic number and header
+            if (    (recvBuf[0] & 0xff) != 0xff ||
+                    (recvBuf[1] & 0xff) != 0xff ||
+                    (recvBuf[2] & 0xff) != 0xff ||
+                    (recvBuf[3] & 0xff) != 0xff ||
+                    (recvBuf[4] & 0xff) != 0x44) {
+                // Invalid responce from server
+                return null;
+            }
+
+            int playerCount = recvBuf[5];
+            String[] players = new String[playerCount];
+
+            int segmentStart = 7;
+            for (int i=0; i<playerCount; i++) {
+                int segmentLength = getSegmentLength(recvBuf, segmentStart, recvBufLength);
+                players[i] = new String(recvBuf, segmentStart, segmentLength, "UTF-8");
+                segmentStart += segmentLength + 10;
+            }
+
+            return players;
+        } catch (Exception e) {
+            return null;
+        }
+    }
+}
---
+++ app/src/main/java/rikka/srcdsutil/common/ServerObject.java
@@ -0,0 +1,59 @@
+package rikka.srcdsutil.common;
+
+/**
+ * Created by Rikka0w0 on 2/13/2018.
+ */
+
+public class ServerObject{
+    // Saved data
+    public final String hostname;
+    public final int port;
+    public String password = "";
+
+    // Sensed data
+    public boolean contacted = false;
+
+    // Display data
+    public final String text;
+    public String subText;
+
+    public ServerObject(String url) {
+        int pos = url.indexOf(':');
+        if (pos >= 0) {
+            this.hostname = url.substring(0, pos);
+            String portString = url.substring(pos + 1);
+            int port;
+            try {
+                port = Integer.parseInt(portString);
+            } catch (Exception e) {
+                port = 27015;
+            }
+            this.port = port;
+        } else {
+            this.hostname = url;
+            this.port = 27015;
+        }
+
+        this.text = hostname + ":" + String.valueOf(port);
+    }
+
+    public ServerObject(String hostname, int port) {
+        this.hostname = hostname;
+        this.port = port;
+
+        this.text = hostname + ":" + String.valueOf(port);
+    }
+
+    @Override
+    public String toString() {
+        return hostname + ":" + String.valueOf(port);
+    }
+
+    @Override
+    public boolean equals(Object in) {
+        if (!(in instanceof ServerObject))
+            return false;
+
+        return this.toString().equals(in.toString());
+    }
+}
